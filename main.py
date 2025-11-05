57e66ebf0fe45d39a03181f476722e12690bec16
gloptyu <csunni1008@gmail.com>
2025-11-05 09:26:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ b=st.selectbox('좋아하는 음식을 선택하세요!',['이차현이먹고남
 if st.button('인사말 생성'):
   st.info(a+'님,안녕하세요! 반갑습니다!')
   st.warning(b+'를 좋아하냐 취향꼬라지')
-st.erroe('어쩌라고')
+  st.error('어쩌라고')
+  st.balloons()
